005da3691c49cc130a8836351f76c74e04d0947b
Victor Nogueira <victornogu80@gmail.com>
2023-02-19 13:42:01 -0300

    Organizando código

--- snake.py
+++ snake.py
@@ -14,9 +14,37 @@ def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
 
+def snake_movements(my_direction, snake):
+    switcher = {
+        K_UP: (snake[0][0], snake[0][1] - 10),
+        K_DOWN: (snake[0][0], snake[0][1] + 10),
+        K_RIGHT: (snake[0][0] + 10, snake[0][1]),
+        K_LEFT: (snake[0][0] - 10, snake[0][1])
+    }
+
+    snake[0] = switcher.get(my_direction, switcher.get(K_LEFT))
+
+
+def user_commands(event):
+    if event.type == KEYDOWN:
+        if event.key == K_UP or event.key == ord('w'):
+            return K_UP
+        if event.key == K_DOWN or event.key == ord('s'):
+            return K_DOWN
+        if event.key == K_RIGHT or event.key == ord('d'):
+            return K_RIGHT
+        if event.key == K_LEFT or event.key == ord('a'):
+            return K_LEFT
+
+
+def is_hit_the_wall(pos):
+    return pos[0] < 0 or pos[0] > constant.GAME_WIDTH or pos[1] < 0 or pos[1] > constant.GAME_HEIGHT
+
+
 pygame.init()
 screen = pygame.display.set_mode((constant.GAME_WIDTH, constant.GAME_HEIGHT))
 pygame.display.set_caption(constant.GAME_TITLE)
+
 font = pygame.font.Font(None, 50)
 
 snake = [(200, 200), (210, 200), (220, 200)]
@@ -38,14 +66,7 @@ while True:
             pygame.quit()
 
         if event.type == KEYDOWN:
-            if event.key == K_UP or event.key == ord('w'):
-                my_direction = K_UP
-            if event.key == K_DOWN or event.key == ord('s'):
-                my_direction = K_DOWN
-            if event.key == K_RIGHT or event.key == ord('d'):
-                my_direction = K_RIGHT
-            if event.key == K_LEFT or event.key == ord('a'):
-                my_direction = K_LEFT
+            my_direction = user_commands(event)
 
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
@@ -54,24 +75,14 @@ while True:
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
-    if my_direction == K_UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)
-
-    if my_direction == K_DOWN:
-        snake[0] = (snake[0][0], snake[0][1] + 10)
-
-    if my_direction == K_RIGHT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])
-
-    if my_direction == K_LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
+    snake_movements(my_direction, snake)
 
     screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
 
     for pos in snake:
         screen.blit(snake_skin, pos)
-        if pos[0] < 0 or pos[0] > constant.GAME_WIDTH or pos[1] < 0 or pos[1] > constant.GAME_HEIGHT:
+        if is_hit_the_wall(pos):
             print(constant.GAME_OVER_MESSAGE)
             pygame.quit()
 
