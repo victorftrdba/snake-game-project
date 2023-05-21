18b1e3715b3ceae9a8c019e0f03fa39c2f82c45e
Victor Nogueira <victornogu80@gmail.com>
2023-05-21 12:59:50 -0300

    Criação de requirements e formatação do código

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ import constant
 def on_grid_random():
     x = random.randint(0, 620)
     y = random.randint(0, 460)
-    return (x//10 * 10, y//10 * 10)
+    return (x // 10 * 10, y // 10 * 10)
 
 
 def collision(c1, c2):
@@ -19,7 +19,7 @@ def snake_movements(my_direction, snake):
         K_UP: (snake[0][0], snake[0][1] - 10),
         K_DOWN: (snake[0][0], snake[0][1] + 10),
         K_RIGHT: (snake[0][0] + 10, snake[0][1]),
-        K_LEFT: (snake[0][0] - 10, snake[0][1])
+        K_LEFT: (snake[0][0] - 10, snake[0][1]),
     }
 
     snake[0] = switcher.get(my_direction, switcher.get(K_LEFT))
@@ -27,18 +27,23 @@ def snake_movements(my_direction, snake):
 
 def user_commands(event):
     if event.type == KEYDOWN:
-        if event.key == K_UP or event.key == ord('w'):
+        if event.key == K_UP or event.key == ord("w"):
             return K_UP
-        if event.key == K_DOWN or event.key == ord('s'):
+        if event.key == K_DOWN or event.key == ord("s"):
             return K_DOWN
-        if event.key == K_RIGHT or event.key == ord('d'):
+        if event.key == K_RIGHT or event.key == ord("d"):
             return K_RIGHT
-        if event.key == K_LEFT or event.key == ord('a'):
+        if event.key == K_LEFT or event.key == ord("a"):
             return K_LEFT
 
 
 def is_hit_the_wall(pos):
-    return pos[0] < 0 or pos[0] > constant.GAME_WIDTH or pos[1] < 0 or pos[1] > constant.GAME_HEIGHT
+    return (
+        pos[0] < 0
+        or pos[0] > constant.GAME_WIDTH
+        or pos[1] < 0
+        or pos[1] > constant.GAME_HEIGHT
+    )
 
 
 pygame.init()
